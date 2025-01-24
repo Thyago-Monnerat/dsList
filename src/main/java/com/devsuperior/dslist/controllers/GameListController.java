b2176646c8a5a917d5dc912ba79af504160bd424
Thyago-Monnerat <thy_thyago2.5@hotmail.com>
2025-01-23 22:26:44 -0300

    feat: add method to change game position in GameListService feat: add replacementDTO to receive GameData for position switch feat: add SQL query in GameListRepository to update game position feat: add POST endpoint lists/{id}/replacement for switching game positions

--- src/main/java/com/devsuperior/dslist/controllers/GameListController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameListController.java
@@ -2,14 +2,12 @@ package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dtos.GameListDTO;
 import com.devsuperior.dslist.dtos.GameMinDTO;
+import com.devsuperior.dslist.dtos.ReplacementDTO;
 import com.devsuperior.dslist.services.GameListService;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,13 @@ public class GameListController {
     @GetMapping("{id}/games")
     public ResponseEntity<List<GameMinDTO>> getGameList(@PathVariable Long id) {
         return ResponseEntity.ok(gameService.findByList(id));
+    }
+
+    @PostMapping("{id}/replacement")
+    public ResponseEntity<String> move(@PathVariable Long id, @RequestBody ReplacementDTO replacementDTO) {
+        gameListService.move(id, replacementDTO.getSourceIndex(), replacementDTO.getDestinationIndex());
+
+        return ResponseEntity.ok("\"Alteração feita com sucesso\"");
     }
 
 }
